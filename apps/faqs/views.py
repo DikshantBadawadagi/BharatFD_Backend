629ca8e9411053499245bced6719a6a4d4c210d8
Dikshant <dikshantbadawadagi@gmail.com>
2025-02-01 17:40:07 +0530

    Created models & added views

---
+++ apps/faqs/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.conf import settings
+from .models import FAQ
+from .serializers import FAQSerializer
+
+class FAQViewSet(viewsets.ModelViewSet):
+    queryset = FAQ.objects.all()
+    serializer_class = FAQSerializer
+
+    def get_queryset(self):
+        return FAQ.objects.all().order_by('-created_at')
+
+    @method_decorator(cache_page(settings.CACHE_TIMEOUT))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @action(detail=False, methods=['get'])
+    def languages(self, request):
+        return Response({
+            'supported_languages': ['en', 'hi', 'bn'],
+            'default_language': 'en'
+        })
